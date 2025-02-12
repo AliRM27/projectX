330a7b14dbe6d32b6d791e1228e7b311feb319c4
Ali Mammadov <amammadov097@gmail.com>
2025-02-12 20:29:55 +0100

    added dotenv

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,18 @@
+import d from "dotenv"
 import e, { json } from "express";
+d.config()
+
+const PORT = process.env.PORT || 4444
 const app = e()
 
 app.use(json())
+
 app.get("/", (req, res) => {
     console.log(req.body.name)
     res.send("Nice")
 })
 
-app.listen("4444", (err) => {
+app.listen(PORT, (err) => {
     if(err){
         return console.log(err)
     }
