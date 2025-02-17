d1f8faae3758b3b1436a6ec89e275b5aa54acaa8
Ali Mammadov <amammadov097@gmail.com>
2025-02-17 21:22:05 +0100

    add cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import d from "dotenv";
 import e, { json } from "express";
 import connectDB from "./config/db.js";
 import router from "./routes/authRoutes.js";
+import cors from "cors";
 d.config();
 connectDB();
 
@@ -9,7 +10,9 @@ const PORT = process.env.PORT || 4444;
 const app = e();
 
 app.use(json());
+app.use(cors());
 
+//register
 app.use("/", router);
 
 app.listen(PORT, (err) => {
