654a7d8fdb924f0d2c7b9534b0661a713849ae49
Ali Mammadov <amammadov097@gmail.com>
2025-03-15 22:34:50 +0100

    new Tab

--- frontend/app/(tabs)/_layout.js
+++ frontend/app/(tabs)/_layout.js
@@ -72,3 +72,47 @@ const styles = StyleSheet.create({
     elevation: 0,
   },
 });
+// export default function Layout() {
+//   return (
+//     <Tabs
+//       screenOptions={({ route }) => ({
+//         tabBarShowLabel: false,
+//         tabBarStyle: styles.tabBar,
+//         tabBarIcon: ({ focused }) => {
+//           let iconName = route.name === "index" ? "home" : "person";
+
+//           return (
+//             <View style={[styles.iconContainer, focused && styles.activeTab]}>
+//               <Ionicons
+//                 name={iconName}
+//                 size={24}
+//                 color={focused ? "white" : "black"}
+//               />
+//             </View>
+//           );
+//         },
+//       })}
+//     >
+//       <Tabs.Screen name="index" options={{ title: "Home" }} />
+//       <Tabs.Screen name="profile" options={{ title: "Profile" }} />
+
+//     </Tabs>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   tabBar: {
+//     backgroundColor: "white",
+//     height: 60,
+//     borderTopLeftRadius: 20,
+//     borderTopRightRadius: 20,
+//     position: "absolute",
+//   },
+//   iconContainer: {
+//     padding: 10,
+//     borderRadius: 25, // Ensures round shape
+//   },
+//   activeTab: {
+//     backgroundColor: "black", // Active tab background
+//   },
+// });
